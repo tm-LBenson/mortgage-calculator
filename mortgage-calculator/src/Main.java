27dd6ee98b5149aecf6943669559fa4119c65b06
Lewis Benson <tm.lewis.benson@gmail.com>
2022-11-23 18:20:28 -0500

    add error handling and input validation

--- mortgage-calculator/src/Main.java
+++ mortgage-calculator/src/Main.java
@@ -2,53 +2,52 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    private static final byte MONTHS_IN_YEAR = 12;
+    private static final byte PERCENT = 100;
+
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-        Scanner scanner = new Scanner(System.in);
-        int principal;
-        while(true) {
-            System.out.println("Choose principal between 1,000 and 1,000,000");
-            System.out.printf("Principal ");
-             principal = scanner.nextInt();
-             if(principal >= 1000 && principal <= 1_000_000){
-                 break;
-             }else{
-                 System.out.println("Invalid");
-             }
-        }
-        float interest;
-        while (true) {
-            System.out.println("Choose interest rate between 1% and 30%");
-            System.out.printf("Annual Interest Rate: ");
+        int principal = readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = readNumber("Annual Interest Rate: ", 1f, 30f);
+        byte years = (byte) readNumber("Period (Years): ", 1, 30);
 
-            interest = scanner.nextFloat();
-            if(interest > 0 && interest <= 30){
-                break;
-            }else{
-                System.out.println("invalid");
-            }
-        }
-        byte period;
-        while(true){
-            System.out.println("Duration of loan between 1 and 30 years");
-            System.out.printf("Period (years) ");
-            period = scanner.nextByte();
-            if(period > 1 && period <= 30){
-                break;
-            }else{
-                System.out.println("invalid");
-            }
-        }
-
-        NumberFormat formatter = NumberFormat.getCurrencyInstance();
-        double monthlyInterest = (interest /PERCENT)/MONTHS_IN_YEAR;
-        double numberOfPayments = period * MONTHS_IN_YEAR;
-        double numerator = monthlyInterest * Math.pow((1 + monthlyInterest), numberOfPayments);
-        double denominator = Math.pow((1 + monthlyInterest), numberOfPayments) - 1;
-        double reducedFraction = principal*(numerator/denominator);
-        String mortgageFormatted = formatter.format(reducedFraction);
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
+    }
 
+    private static int readNumber(String prompt, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextInt();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Please enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+    private static float readNumber(String prompt, float min, float max) {
+        Scanner scanner = new Scanner(System.in);
+        float value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Please enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+    private static double calculateMortgage(int principal, float annualInterest, byte years) {
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double numerator = monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments);
+        double denominator = Math.pow(1 + monthlyInterest, numberOfPayments) - 1;
+
+        return principal * (numerator / denominator);
     }
 }
